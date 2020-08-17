5a9afba4f4291b2857f1c32dc62ce65e72f0507a
Samuel Romero Arbelo <oiranca@gmail.com>
2020-08-17 20:25:11 +0100

    New logo and create map id

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ let priceElement = 0;
 let count = 0;
 let total = 0;
 let imagen = new Map();
-
 const backet = document.getElementById("cesta");
 let imagenes = document.getElementsByTagName('img');
 
@@ -14,6 +13,7 @@ for (let img of imagenes) {
     document.getElementById(img.id).setAttribute('draggable', 'true')
 
     document.getElementById(img.id).addEventListener('dragstart', (e) => {
+      count = 0;
       e.dataTransfer.setData('text', e.target.id);
 
 
@@ -33,12 +33,28 @@ backet.addEventListener('dragover', (e) => {
 });
 
 backet.addEventListener('drop', (e) => {
+  const idElement= e.dataTransfer.getData('text');
+  count++;
+
+  if (imagen.has(idElement)){
+    let countItemsShops=imagen.get(idElement);
+    count+=countItemsShops;
+    imagen.set(idElement,count);
+    
+  }else{
+    imagen.set(idElement, count);
+  }
+
+    
+  
+
   let price;
   e.preventDefault();
-  let parent = document.getElementById(e.dataTransfer.getData('text')).parentNode.id;
+  let parent = document.getElementById(idElement).parentNode.id;
   price = document.getElementById(parent).querySelector("p.price").textContent;
   total = Number(price.replace("€", "")) + total;
-  document.getElementById("total").innerText = (total+'€');
+  document.getElementById("total").innerText = (total + '€');
+  
 
 })
 
