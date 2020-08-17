7c6532c26043309a3fb36a89269d0c13a1650d40
Samuel Romero Arbelo <oiranca@gmail.com>
2020-08-17 19:11:06 +0100

    Drag an drop element with sum of price

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,48 @@
 
+let priceElement = 0;
+let count = 0;
 let total = 0;
+let imagen = new Map();
 
 const backet = document.getElementById("cesta");
-const producto = document.getElementsByClassName('stock');
+let imagenes = document.getElementsByTagName('img');
 
-for (let items of producto) {
-  let imagenes = items.getElementsByTagName('img');
+for (let img of imagenes) {
 
-  for (let img of imagenes) {
-    document.getElementById(img.id).setAttribute('draggable', 'true');
+  if (img.id != 'backetShop') {
+
+    document.getElementById(img.id).setAttribute('draggable', 'true')
 
     document.getElementById(img.id).addEventListener('dragstart', (e) => {
-       
-      
-      let price = items.querySelector("p.price").textContent;
-  
-      total = Number(price.replace("€", "")) + total;
-  
-      document.getElementById("total").innerText = (total);
-  
-      let imagen = items.querySelector('img');
-  
-  
-    })
+      e.dataTransfer.setData('text', e.target.id);
 
+
+    });
+
+  } else {
+    document.getElementById(img.id).setAttribute('draggable', 'false');
   };
-
- 
-
-  
 
 
 
 };
+
+
+backet.addEventListener('dragover', (e) => {
+  e.preventDefault();
+});
+
+backet.addEventListener('drop', (e) => {
+  let price;
+  e.preventDefault();
+  let parent = document.getElementById(e.dataTransfer.getData('text')).parentNode.id;
+  price = document.getElementById(parent).querySelector("p.price").textContent;
+  total = Number(price.replace("€", "")) + total;
+  document.getElementById("total").innerText = (total+'€');
+
+})
+
+
 
 
 
